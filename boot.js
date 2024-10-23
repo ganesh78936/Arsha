7e39fd75923e19444ed319526798a1c6e80b0c2c
ganesh78936 <pilliganeshkumarreddy@gmail.com>
2024-10-23 18:42:43 +0530

    Add files via upload
    no

---
+++ boot.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.your-class').slick({
+        
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 1800,
+          
+    });
+  });
+  
+  $(document).ready(function(){
+        $('.sujj').slick({
+            
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 1800,
+              
+        });
+      });
